febed5988525c8232c993af398e891825d430f48
Gabriel amaral <amaralgabriel357@gmail.com>
2026-03-29 09:11:29 -0300

    feat: configura crud inicial do FoodItem

--- MagicFridgeAI/src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
+++ MagicFridgeAI/src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
@@ -4,11 +4,9 @@ import dev.java10x.MagicFridgeAI.dto.FoodItemDTO;
 import dev.java10x.MagicFridgeAI.service.FoodItemService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/food")
@@ -20,8 +18,40 @@ public class FoodItemController {
         this.foodItemService = foodItemService;
     }
 
+    @PostMapping("/criar")
     public ResponseEntity<FoodItemDTO> criar(@RequestBody FoodItemDTO foodItem) {
-      FoodItemDTO foodItemDTO = foodItemService.salvar(foodItem);
-      return ResponseEntity.status(HttpStatus.CREATED).body(foodItemDTO);
+        FoodItemDTO foodItemDTO = foodItemService.criar(foodItem);
+        return ResponseEntity.status(HttpStatus.CREATED).body(foodItemDTO);
     }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<FoodItemDTO>> listar() {
+        List<FoodItemDTO> foodItemDTO = foodItemService.listar();
+        return ResponseEntity.status(HttpStatus.OK).body(foodItemDTO);
+    }
+
+    @GetMapping("/listarPorId/{id}")
+    public ResponseEntity<?> listarPorId(@PathVariable Long id) {
+        FoodItemDTO foodItemDTO = foodItemService.listarPorId(id);
+
+        if (foodItemDTO != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(foodItemDTO);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Igrediente não encontrado");
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletar(@PathVariable Long id) {
+        if (foodItemService.listarPorId(id) != null) {
+            foodItemService.deletarItem(id);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Item com o id " + id + " deletado com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("nenhum igrediente encontrado");
+        }
+    }
+
 }
--- MagicFridgeAI/src/main/java/dev/java10x/MagicFridgeAI/service/FoodItemService.java
+++ MagicFridgeAI/src/main/java/dev/java10x/MagicFridgeAI/service/FoodItemService.java
@@ -5,7 +5,10 @@ import dev.java10x.MagicFridgeAI.mapper.FoodItemMapper;
 import dev.java10x.MagicFridgeAI.model.FoodItemModel;
 import dev.java10x.MagicFridgeAI.repository.FoodItemRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class FoodItemService {
@@ -18,9 +21,25 @@ public class FoodItemService {
         this.foodItemMapper = foodItemMapper;
     }
 
-    public FoodItemDTO criar(@RequestBody FoodItemDTO foodItemDTO) {
+    public FoodItemDTO criar(FoodItemDTO foodItemDTO) {
         FoodItemModel foodItemModel = foodItemMapper.map(foodItemDTO);
         foodItemModel = foodItemRepository.save(foodItemModel);
         return foodItemMapper.mapToDTO(foodItemModel);
+    }
+
+    public List<FoodItemDTO> listar() {
+        List<FoodItemModel> foodItem = foodItemRepository.findAll();
+        return foodItem.stream()
+                .map(foodItemMapper::mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public FoodItemDTO listarPorId(Long id) {
+        Optional<FoodItemModel> foodItem = foodItemRepository.findById(id);
+        return foodItem.map(foodItemMapper::mapToDTO).orElse(null);
+    }
+
+    public void deletarItem(Long id) {
+        foodItemRepository.deleteById(id);
     }
 }
